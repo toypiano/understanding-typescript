9dc473f273e7dad2fe6f2e1fc224ff51e6e63261
Hayoun Lee <sidhlee@gmail.com>
2020-05-02 08:12:29 +0900

    moved to 'codes' folder

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
-
-// Instantiate to render
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('completed');
