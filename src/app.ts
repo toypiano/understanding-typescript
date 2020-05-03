3421a70c0d4653b4e6bc95526f3c577c4a3b9c46
Hayoun Lee <sidhlee@gmail.com>
2020-05-04 08:55:07 +0900

    Move to new folder

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
-import { ProjectStatus } from './models/project';
-
-new ProjectInput();
-new ProjectList(ProjectStatus.Active);
-new ProjectList(ProjectStatus.Completed);
